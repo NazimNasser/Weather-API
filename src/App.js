8c3706fdf7ae99e3d01afa43b3af7461b63607a7
Nazim Nasser <nazimnasser94@gmail.com>
2021-12-17 15:20:21 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
+// import SayHi, { SayHello } from "./components/WeatherItem";
+// import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
 
@@ -21,10 +21,69 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+
+        <header className="container-header">
+          <div className="container-input">
+            <input type="text" placeholder="Type in a city name">
+            </input>
+            <button>FIND WEATHER</button>
+          </div>
+        </header>
+        <div className="main-weather">
+            <div className="weather-today">
+              <img src="https://via.placeholder.com/170x150" alt="cloud-img"/>
+              <p><span className="overcast">overcast clouds</span></p>
+              <p><span className="spacing">Temperature</span> 10&#176; to 11&#176; C</p>
+              <p className="change-sizing"><span>Humidity</span> 78% <span>Pressure</span> 1008.48</p>
+            </div>
+
+            <div className="week-weather">
+              <div className="item">
+                <time>03:00</time>
+                <img src="https://via.placeholder.com/140x100" alt="img1"/>
+                <p>8 &#176;C</p>
+              </div>
+
+              <div className="item"> 
+                <time>06:00</time>
+                <img src="https://via.placeholder.com/140x100" alt="img2"/>
+                <p>9 &#176;C</p>
+              </div>
+
+              <div className="item">
+                <time>09:00</time>
+                <img src="https://via.placeholder.com/140x100" alt="img3"/>
+                <p>14 &#176;C</p>
+              </div>
+
+              <div className="item"> 
+                <time>12:00</time>
+                <img src="https://via.placeholder.com/140x100" alt="img4"/>
+                <p>17 &#176;C</p>
+              </div>
+
+              <div className="item">
+                <time>15:00</time>
+                <img src="https://via.placeholder.com/140x100" alt="img5"/>
+                <p>18 &#176;C</p>
+              </div>
+
+              <div className="item">
+                <time>18:00</time>
+                <img src="https://via.placeholder.com/140x100" alt="img6"/>
+                <p>16 &#176;C</p>
+              </div>
+
+              <div className="item">
+                <time>21:00</time>
+                <img src="https://via.placeholder.com/140x100" alt="img7"/>
+                <p>13 &#176;C</p>
+              </div>
+            </div>
+
+          </div>
       </div>
+
     );
   }
 }
